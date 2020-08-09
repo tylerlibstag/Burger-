d553ce1f31e279f351b07e60c81fce3547c593a5
TYTY <tylerlibstag@gmail.com>
2020-08-09 18:31:11 -0400

    added layouts and html

--- db/schema.sql
+++ db/schema.sql
@@ -7,5 +7,6 @@ USE burger_db;
 CREATE TABLE burger(
     id INT UNSIGNED AUTO_INCREMENT,
     burger_name VARCHAR(50) NOT NULL,
-    devoured BOOLEAN NOT NULL
+    devoured BOOLEAN DEFAULT false,
+    PRIMARY KEY (id)
 )
---
+++ models/burger.js
@@ -0,0 +1,22 @@
+var orm = require("../config/orm.js");
+
+
+var burger = {
+    all: function(cb) {
+      orm.all("burger", function(res) {
+        cb(res);
+      });
+    },
+    // The variables cols and vals are arrays.
+    create: function(cols, vals, cb) {
+      orm.create("burger", cols, vals, function(res) {
+        cb(res);
+      });
+    },
+    update: function(objColVals, condition, cb) {
+      orm.update("burger", objColVals, condition, function(res) {
+        cb(res);
+      });
+    }
+};    
+module.exports = burger;
