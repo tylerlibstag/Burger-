d25d9406e0d924dcbcef8362ce986a2c2de22ff1
TYTY <tylerlibstag@gmail.com>
2020-08-10 22:56:42 -0400

    added updated handelbar files

--- config/connection.js
+++ config/connection.js
@@ -1,26 +1,22 @@
+// Set up MySQL connection.
 var mysql = require("mysql");
-require("dotenv").config();
-console.log(process.env.PRODUCTION);
 
-var connection;
-if (process.env.JAWSDB_URL) {
-  
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-  
-    connection = mysql.createConnection(process.env.PRODUCTION);
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "password",
+  database: "burger_db"
+});
 
+// Make connection.
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
 
-  };
-
-  // Make connection.
-  connection.connect(function (err) {
-    if (err) {
-      console.error("error connecting: " + err.stack);
-      return;
-    }
-    console.log("connected as id " + connection.threadId);
-  });
-
-  // Export connection for our ORM to use.
-  module.exports = connection;
+// Export connection for our ORM to use.
+module.exports = connection;
